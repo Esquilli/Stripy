cd1734f005f8b4ae7a578187223e9c8ed64ac4a8
Pedro J. Fernandez <esquillidev@gmail.com>
2022-01-18 17:27:57 -0500

    Initial Commit

---
+++ Stripy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stripy
+//
+//  Created by Pedro Fernandez on 1/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stripy/StripyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StripyApp.swift
+//  Stripy
+//
+//  Created by Pedro Fernandez on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct StripyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
